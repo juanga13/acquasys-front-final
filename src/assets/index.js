f05edff7e2a0a411cdaa6ea01211e343f7ac144c
Juan Gabriel Ricci <riccijuanga@gmail.com>
2020-02-07 09:24:57 -0300

    assets

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import brandLogo from './brand-logo.png'
+import students from './students.png'
+import teachers from './teachers.png'
+import lessons from './lessons.png'
+import payments from './payments.png'
+import dummyAvatar from './dummyAvatar.png'
+
+export { brandLogo, students, teachers, lessons, payments, dummyAvatar }
